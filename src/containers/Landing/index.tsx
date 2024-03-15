c2a91565009d506ca332d06583049970f31913fc
Brian Kheng <briankheng63@gmail.com>
2024-03-15 15:20:22 +0800

    fix: revert landing page

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -1,82 +1,19 @@
-"use client";
+"use client"
 import React from "react";
+import HeaderLanding from "@/components/Header/HeaderLanding";
 import { useTranslations } from "next-intl";
 import { useAppSelector } from "@/redux";
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
-
-const { Sider, Content, Footer } = Layout;
-
-const items2: MenuProps["items"] = [
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-].map((icon, index) => {
-  const key = String(index + 1);
-
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
 
 const LandingContainer: React.FC = () => {
-  // const { user } = useAppSelector((state) => state.auth);
-  // const t = useTranslations("Landing");
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
+  const { user } = useAppSelector((state) => state.auth);
+  const t = useTranslations('Landing');
   return (
-    <Layout className="h-full">
-      <Layout>
-        <Sider width={200} style={{ background: colorBgContainer }}>
-          <div className="bg-zinc-400 w-full h-[10%] text-white flex justify-center items-center"> LOGO </div>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "90%", borderRight: 0 }}
-            items={items2}
-          />
-        </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            Content
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            ITS ©{new Date().getFullYear()} Created by Your Peanut Butter X Jelly Sandwich
-          </Footer>
-        </Layout>
-      </Layout>
-    </Layout>
+    <div>
+      <HeaderLanding />
+      <div className="text-4xl font-bold flex justify-center">
+        LANDING PAGE: <strong>{t("title")}</strong> ! {user.email}
+      </div>
+    </div>
   );
 };
 
