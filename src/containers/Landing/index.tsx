f3552e9cda16cad7ac8c3855ff6b7cd2200cb0ae
Emrysil <ctzszty@gmail.com>
2024-03-18 15:01:21 +0800

    redirect user to their role landing page after they've logged in and trying to access the generic landing page

--- src/containers/Landing/index.tsx
+++ src/containers/Landing/index.tsx
@@ -2,9 +2,30 @@
 import React from "react";
 import HeaderLanding from "@/components/Header/HeaderLanding";
 import { useTranslations } from "next-intl";
-
+import { useAppSelector } from "@/redux";
+import type { RootState } from "@/redux";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import getLocale from "@/common/utils/extractLocale";
 const LandingContainer: React.FC = () => {
   const t = useTranslations("Landing");
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = getLocale(pathname);
+  const { isAuthenticated, user } = useAppSelector(
+    (state: RootState) => state.auth,
+  );
+  useEffect(() => {
+    if (isAuthenticated) {
+      if (user.is_manager) {
+        router.push(`\\${locale}\\manager`);
+      } else if (user.is_tutor) {
+        router.push(`\\${locale}\\tutor`);
+      } else {
+        router.push(`\\${locale}\\student`);
+      }
+    }
+  });
   return (
     <div className="h-screen flex flex-col">
       <HeaderLanding />
